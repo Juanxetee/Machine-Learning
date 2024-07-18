06406fc6a6db04ba504dfb6702c357c82d01b61d
Juanxetee <buquerin6@hotmail.com>
2024-07-18 23:49:15 +0200

    Terminado
    Listo para entregar, tras subir el modelo no podré hacer más push

--- ML_ValoraOpinion/Streamlit.py
+++ ML_ValoraOpinion/Streamlit.py
@@ -4,10 +4,11 @@ import pickle
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+# Cargar el modelo y el vectorizador
 with open('Modelo/best_modelfinal_rf.pkl', 'rb') as file:
     best_model_rf = pickle.load(file)
     
-with open('vectorizer.pkl', 'rb') as file:
+with open('Modelo/vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
 
 # Función para predecir el comentario
@@ -18,23 +19,27 @@ def predecir_comentario():
         prediccion = best_model_rf.predict(comentario_vectorizado)
         resultado = "Positivo" if prediccion[0] == 1 else "Negativo"
         messagebox.showinfo("Resultado de la Predicción", f"El comentario es {resultado}")
+        entrada_comentario.delete("1.0", tk.END)  # Limpiar el campo de entrada después de la predicción
     else:
         messagebox.showwarning("Entrada Vacía", "Por favor ingrese un comentario.")
 
 # Crear la interfaz gráfica
 ventana = tk.Tk()
 ventana.title("Clasificación de Comentarios")
+ventana.geometry("800x600")  # Ajustar el tamaño de la ventana
+ventana.configure(bg='black')  # Fondo negro
 
 # Etiqueta y campo de entrada para el comentario
-etiqueta_comentario = tk.Label(ventana, text="Ingrese un comentario:")
-etiqueta_comentario.pack(pady=5)
+etiqueta_comentario = tk.Label(ventana, text="Ingrese un comentario:", font=("Helvetica", 14), fg="white", bg="black")
+etiqueta_comentario.pack(pady=10)
 
-entrada_comentario = tk.Text(ventana, height=10, width=50)
-entrada_comentario.pack(pady=5)
+entrada_comentario = tk.Text(ventana, height=10, width=70, font=("Helvetica", 12), bg="black", fg="white", insertbackground="white")
+entrada_comentario.pack(pady=10)
 
 # Botón para realizar la predicción
-boton_predecir = tk.Button(ventana, text="Predecir", command=predecir_comentario)
-boton_predecir.pack(pady=5)
+boton_predecir = tk.Button(ventana, text="Predecir", font=("Helvetica", 12, "bold"), bg="#ff6347", fg="white", command=predecir_comentario)
+boton_predecir.pack(pady=10)
 
 # Ejecutar la interfaz gráfica
 ventana.mainloop()
+
